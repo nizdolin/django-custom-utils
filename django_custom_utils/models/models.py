46df6df633c8af0d4f15d1270514efdd849e0997
nizdolin <nizdolin@gmail.com>
2023-08-09 18:24:13 +0300

    base Django utils

---
+++ django_custom_utils/models/models.py
@@ -0,0 +1,89 @@
+import re
+
+from django.contrib.contenttypes.models import ContentType
+from django.db import models, ProgrammingError, OperationalError
+from django.urls import reverse
+from django.utils.functional import classproperty
+from django.utils.html import conditional_escape, format_html
+
+from django_custom_utils.models.managers import ActiveManager
+from django_custom_utils import utils
+
+
+__all__ = [
+    'BaseModel',
+    'CreatedAtModel',
+    'DateTimeModel',
+    'ActiveModel',
+]
+
+
+class BaseModel(models.Model):
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+    @classproperty
+    def app_label(cls):  # noqa
+        return cls._meta.app_label  # noqa
+
+    @classproperty
+    def model_name(cls):  # noqa
+        return cls._meta.model_name  # noqa
+
+    @classproperty
+    def verbose_name(cls):  # noqa
+        return cls._meta.verbose_name  # noqa
+
+    @classproperty
+    def verbose_name_plural(cls):  # noqa
+        return re.sub('^\d+\. ', '', cls._meta.verbose_name_plural)  # noqa
+
+    @classmethod
+    def get_index_url(cls, **kwargs):
+        path = reverse(f'admin:{cls.app_label}_{cls.model_name}_changelist')
+        return utils.site_url(path, **kwargs)
+
+    @utils.cached_classproperty
+    def content_type_id(cls):  # noqa
+        try:
+            return ContentType.objects.get_for_model(cls).id
+        except (ProgrammingError, OperationalError):
+            return
+
+    def get_admin_url(self, **kwargs):
+        path = reverse(f'admin:{self.app_label}_{self.model_name}_change', args=[self.pk])
+        return utils.site_url(path, **kwargs)
+
+    def get_admin_link(self, title='', attr=None):
+        if not self.pk:
+            return '-'
+        if not title and attr and hasattr(self, attr):
+            title = getattr(self, attr)
+        title = title or str(self)
+        return format_html('<a href="{}" target="_blank">{}</a>', self.get_admin_url(), conditional_escape(title))
+
+
+class CreatedAtModel(BaseModel):
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, verbose_name='Создано')
+
+    class Meta:
+        abstract = True
+
+
+class DateTimeModel(CreatedAtModel):
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
+
+    class Meta:
+        abstract = True
+
+
+class ActiveModel(BaseModel):
+    is_active = models.BooleanField(default=True, verbose_name='Активно?')
+
+    objects = models.Manager()
+    active_objects = ActiveManager()
+
+    class Meta:
+        abstract = True
---
+++ django_custom_utils/utils.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.http import QueryDict
+from django.utils.functional import classproperty
+
+
+def site_url(path: str, **kwargs) -> str:
+    query_string = ''
+    if kwargs:
+        q = QueryDict('', mutable=True)
+        for k, v in kwargs.items():
+            if v is not None and v != '':
+                q[k] = v
+        query_string = '?' + q.urlencode()
+    return settings.BASE_URL + path + query_string
+
+
+class cached_classproperty(classproperty):
+    def get_result_field_name(self):
+        return self.fget.__name__ + "_property_result" if self.fget else None
+
+    def __get__(self, instance, cls=None):
+        result_field_name = self.get_result_field_name()
+
+        if hasattr(cls, result_field_name):
+            return getattr(cls, result_field_name)
+
+        if not cls or not result_field_name:
+            return self.fget(cls)
+
+        setattr(cls, result_field_name, self.fget(cls))
+        return getattr(cls, result_field_name)
